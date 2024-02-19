6c2285e6717504e6305d88c15707ebf93046966f
Omar Mohamed <omarcoptan9@gmail.com>
2024-02-19 20:18:33 +0000

    lavender: RisingOS Official

--- rising_lavender.mk
+++ rising_lavender.mk
@@ -9,13 +9,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rising/config/rising.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Rising flags
+RISING_MAINTAINER := Coptan
+WITH_GMS := true
+TARGET_CORE_GMS := true
+RISING_CHIPSET := SDM660
+SUSHI_BOOTANIMATION := 1080
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
 # Build flags
-PRODUCT_NAME := lineage_lavender
+PRODUCT_NAME := rising_lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
